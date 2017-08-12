a281e214911a5545878508f7fed04f61d1c1b74b
ArtemeeSenin <artemee.senin@gmail.com>
2017-08-12 14:42:46 +0300

    global object basic setup main.css error class added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+/*Validation*/
 
+var MyForm = {
+    validate: function() {
+        return {isValid: false, error: ['hello']}
+    },
+    getData: function(data){
+        return this[data];
+    },
+    setData: function(){},
+    submit: function(){}
+};
