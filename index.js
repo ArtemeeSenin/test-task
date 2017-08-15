e049e38cda17bb6cf5abe574733e615c16c2c30a
ArtemeeSenin <artemee.senin@gmail.com>
2017-08-15 23:34:21 +0300

    regex for checking validation and object methods set up

--- index.js
+++ index.js
@@ -2,11 +2,32 @@
 
 var MyForm = {
     validate: function() {
-        return {isValid: false, error: ['hello']}
+        console.log('validate');
+        var result = {
+            isValid: false,
+            errorFields: []
+        }
+        var fields = Array.from(form.querySelectorAll('input'));
+        var reForm = {
+            fio: /^(([A-Za-zА-Яа-я]+)\ ){2}([A-Za-zА-Яа-я]+){1}$/,
+            email: /^([a-z0-9_.-]+)@((ya\.ru)|(yandex)\.(ru|ua|by|kz|com))$/,
+            phone: /^\+7\([\d]{3}\)[\d]{3}\-[\d]{2}\-[\d]{2}$/
+        }
+
+        return result;
     },
-    getData: function(data){
-        return this[data];
-    },
+    getData: function(){},
     setData: function(){},
-    submit: function(){}
+    submit: function(){
+        console.log('submit');
+        var response = this.validate();
+        console.log(response);
+    }
 };
+
+var form = document.forms.myForm;
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    MyForm.submit();
+});
